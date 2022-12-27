f69230ddf1fccdf276ad1ca3808328ab4ec6b51e
PolinaKrp <karpacevapolina@gmail.com>
2022-12-27 17:50:07 +0400

    Final commit
    Final commit

--- template.cpp
+++ template.cpp
@@ -3,80 +3,60 @@
 #include <math.h>
 #include <iomanip>
 
-//class EClassException
-//{
-//protected:
-//    char _err[256];
-//public:
-//    EClassException(const char* err);
-//    void Print();
-//    ~EClassException();
-//};
-//
-//EClassException::EClassException(const char* err)
-//{
-//    strncpy_s(_err, err, 255);
-//    _err[255] = 0;
-//}
-//void EClassException::Print()
-//{
-//    std::cout << _err << std::endl;
-//}
-//EClassException::~EClassException() {}
+using namespace std;
 
 class EClassException
 {
 protected:
     char _err[256];
 public:
-    EClassException(const char* err);
-    virtual void Print();
-    virtual ~EClassException();
+    EClassException(const char* err)
+    {
+        strncpy_s(_err, err, 255);
+        _err[255] = 0;
+    }
+    void Print()
+    {
+        std::cout << _err << std::endl;
+    }
+    ~EClassException() {};
 };
-
-//EClassException::EClassException(const char* err)
-//{
-//    strncpy_s(_err, err, 255);
-//    _err[255] = 0;
-//}
-void EClassException::Print()
-{
-    std::cout << _err << std::endl;
-}
-EClassException::~EClassException() {}
 
 template <typename T>
 class BinaryImg
 {
 private:
-    T** matrix;
+    T** array;
     int row;
     int col;
-    BinaryImg<T> And(T rhs) const
+
+    BinaryImg<T> And(T rhs) const   
     {
         BinaryImg<T> result(row, col);
         for (int i = 0; i < row; i++)
         {
             for (int j = 0; j < col; j++)
             {
-                result.matrix[i][j] = matrix[i][j] * rhs;
+                result.array[i][j] = array[i][j] * rhs;
             }
         }
         return result;
     }
-    BinaryImg<T> Or(T rhs) const
+
+    BinaryImg<T> Or(T rhs) const //не измен€емый экземпл€р класса
     {
         BinaryImg<T> result(row, col);
         for (int i = 0; i < row; i++)
         {
             for (int j = 0; j < col; j++)
             {
-                result.matrix[i][j] = matrix[i][j] + rhs;
+                result.array[i][j] = array[i][j] + rhs;
             }
         }
         return result;
     }
-    BinaryImg<T> And(const BinaryImg<T>& src) const
+
+    BinaryImg<T> And(const BinaryImg<T>& src) const  //перегрузка методов
     {
         if (row != src.row || col != src.col) { throw EClassException("Invalid dimensions of images"); }
         BinaryImg<T> result(row, col);
@@ -84,11 +64,12 @@ private:
         {
             for (int j = 0; j < col; j++)
             {
-                result.matrix[i][j] = matrix[i][j] * src.matrix[i][j];
+                result.array[i][j] = array[i][j] * src.array[i][j];  
             }
         }
         return result;
     }
+
     BinaryImg<T> Or(const BinaryImg<T>& src) const
     {
         if (row != src.row || col != src.col) { throw EClassException("Invalid dimensions of images"); }
@@ -97,40 +78,41 @@ private:
         {
             for (int j = 0; j < col; j++)
             {
-                result.matrix[i][j] = matrix[i][j] + src.matrix[i][j];
+                result.array[i][j] = array[i][j] + src.array[i][j];
             }
         }
         return result;
     }
 
-
 public:
+
     BinaryImg(int x, int y)
     {
         if (x <= 0 || y <= 0) { throw EClassException("invalid dimension"); }
         row = x;
         col = y;
-        matrix = new T * [row];
+        array = new T * [row];
         for (int i = 0; i < row; i++)
         {
-            matrix[i] = new T[col]{};
+            array[i] = new T[col]{};
         }
     }
-    BinaryImg(const BinaryImg<T>& src)
+
+    BinaryImg(const BinaryImg<T>& src)  //конструктор копир
     {
         row = src.row;
         col = src.col;
-        matrix = new T * [row];
+        array = new T * [row];
         for (int i = 0; i < row; i++)
         {
-            matrix[i] = new T[col];
+            array[i] = new T[col];
         }
 
         for (int i = 0; i < row; i++)
         {
             for (int j = 0; j < col; j++)
             {
-                matrix[i][j] = src.matrix[i][j];
+                array[i][j] = src.array[i][j];
             }
         }
     }
@@ -139,24 +121,27 @@ public:
     {
         for (int i = 0; i < row; i++)
         {
-            delete[]matrix[i];
+            delete[]array[i];
         }
-        delete[]matrix;
+        delete[]array;
     }
-    int GetRow() const { return row; }
+
+    int GetRow() const { return row; } 
     int GetCol() const { return col; }
-    friend std::ostream& operator<<(std::ostream& os, BinaryImg<T>& obj)
+
+    friend ostream& operator<<(std::ostream& os, BinaryImg<T>& obj)
     {
         for (int i = 0; i < obj.row; i++)
         {
             for (int j = 0; j < obj.col; j++)
             {
-                std::cout << obj.matrix[i][j] << std::setw(2);
+                cout << obj.array[i][j] << std::setw(2); //
             }
-            std::cout << "\n";
+            cout << "\n";
         }
         return os;
     }
+
     bool operator==(const BinaryImg<T>& src) const
     {
         if (row != src.row || col != src.col) { return false; }
@@ -164,66 +149,75 @@ public:
         {
             for (int j = 0; j < col; j++)
             {
-                if (matrix[i][j] != src.matrix[i][j]) { return false; }
+                if (array[i][j] != src.array[i][j]) { return false; }
             }
         }
         return true;
     }
+
     bool operator!=(const BinaryImg<T>& src) const
     {
         return !(*this == src);
     }
+
     T& operator()(int x, int y)
     {
         if ((x >= row || y >= col) || (x < 0 || y < 0))
         {
             throw EClassException("invalid index");
         }
-        return matrix[x][y];
+        return array[x][y];
     }
+
     T operator()(int x, int y) const
     {
         if ((x >= row || y >= col) || (x < 0 || y < 0))
         {
             throw EClassException("invalid index");
         }
-        return matrix[x][y];
+        return array[x][y];
     }
-    BinaryImg<T> operator+(const BinaryImg<T>& src) const
+
+    BinaryImg<T> operator+(const BinaryImg<T>& src) const  //оператор сложени€ двух матриц
     {
         return this->Or(src);
     }
+
     BinaryImg<T> operator*(const BinaryImg<T>& src) const
     {
         return this->And(src);
     }
+
     BinaryImg<T> operator+(T rhs) const
     {
         return this->Or(rhs);
     }
+
     BinaryImg<T> operator*(T rhs) const
     {
         return this->And(rhs);
     }
+
     BinaryImg<T> operator!()
     {
         for (int i = 0; i < row; i++)
         {
             for (int j = 0; j < col; j++)
             {
-                if (matrix[i][j] == 0)
+                if (array[i][j] == 0)
                 {
-                    matrix[i][j] = std::numeric_limits<T>::max();
+                    array[i][j] = std::numeric_limits<T>::max();
                 }
                 else
                 {
-                    matrix[i][j] = 0;
+                    array[i][j] = 0;
                 }
             }
         }
         return *this;
     }
-    BinaryImg<T>& operator= (const BinaryImg<T>& src) {
+
+    BinaryImg<T>& operator= (const BinaryImg<T>& src) {  
         if (this == (&src)) { return *this; }
         if (row == src.row && col == src.col)
         {
@@ -231,7 +225,7 @@ public:
             {
                 for (int j = 0; j < col; j++)
                 {
-                    matrix[i][j] = src.matrix[i][j];
+                    array[i][j] = src.array[i][j];
                 }
             }
             return *this;
@@ -239,24 +233,24 @@ public:
 
         for (int i = 0; i < row; i++)
         {
-            delete[]matrix[i];
+            delete[]array[i];
         }
-        delete[]matrix;
+        delete[]array;
 
         row = src.row;
         col = src.col;
 
-        matrix = new T * [row];
+        array = new T * [row];
         for (int i = 0; i < row; i++)
         {
-            matrix[i] = new T[col];
+            array[i] = new T[col];
         }
 
         for (int i = 0; i < row; i++)
         {
             for (int j = 0; j < col; j++)
             {
-                matrix[i][j] = src.matrix[i][j];
+                array[i][j] = src.array[i][j];
             }
         }
         return *this;
@@ -269,28 +263,30 @@ public:
         {
             for (int j = 0; j < col; j++)
             {
-                if (matrix[i][j]) { k++; }
+                if (array[i][j]) { k++; }
             }
         }
         double res = (double)k / (row * col);
         return res;
     }
+
     friend BinaryImg<T> operator*(T rhs, const BinaryImg<T>& src)
     {
         return src.And(rhs);
     }
+
     friend BinaryImg<T> operator+(T rhs, const BinaryImg<T>& src)
     {
-        return src.Or(rhs);
+        return src.Or(rhs);  //прибавл€ем число и возвращаем
     }
 };
 
-
+// Specialization for char
 template <>
 class BinaryImg<char>
 {
 private:
-    char** matrix;
+    char** array;
     int row;
     int col;
     BinaryImg<char> And(char rhs) const
@@ -300,11 +296,13 @@ private:
         {
             for (int j = 0; j < col; j++)
             {
-                result.matrix[i][j] = (int)matrix[i][j] * (int)rhs;
+                result.array[i][j] = (int)array[i][j] * (int)rhs;
             }
         }
         return result;
     }
+
+
     BinaryImg<char> Or(char rhs) const
     {
         BinaryImg<char> result(row, col);
@@ -312,11 +310,12 @@ private:
         {
             for (int j = 0; j < col; j++)
             {
-                result.matrix[i][j] = (int)matrix[i][j] + (int)rhs;
+                result.array[i][j] = (int)array[i][j] + (int)rhs;
             }
         }
         return result;
     }
+
     BinaryImg<char> And(const BinaryImg<char>& src) const
     {
         if (row != src.row || col != src.col) { throw EClassException("Invalid dimensions of images"); }
@@ -325,11 +324,12 @@ private:
         {
             for (int j = 0; j < col; j++)
             {
-                result.matrix[i][j] = (int)matrix[i][j] * (int)src.matrix[i][j];
+                result.array[i][j] = (int)array[i][j] * (int)src.array[i][j];
             }
         }
         return result;
     }
+
     BinaryImg<char> Or(const BinaryImg<char>& src) const
     {
         if (row != src.row || col != src.col) { throw EClassException("Invalid dimensions of images"); }
@@ -338,7 +338,7 @@ private:
         {
             for (int j = 0; j < col; j++)
             {
-                result.matrix[i][j] = (int)matrix[i][j] + (int)src.matrix[i][j];
+                result.array[i][j] = (int)array[i][j] + (int)src.array[i][j];
             }
         }
         return result;
@@ -350,59 +350,64 @@ public:
         if (x <= 0 || y <= 0) { throw EClassException("invalid dimension"); }
         row = x;
         col = y;
-        matrix = new char* [row];
+        array = new char* [row];
         for (int i = 0; i < row; i++)
         {
-            matrix[i] = new char[col];
+            array[i] = new char[col];
         }
         for (int i = 0; i < row; i++)
         {
             for (int j = 0; j < col; j++)
             {
-                matrix[i][j] = '0';
+                array[i][j] = '0';
             }
         }
     }
+
     BinaryImg(const BinaryImg<char>& src)
     {
         row = src.row;
         col = src.col;
-        matrix = new char* [row];
+        array = new char* [row];
         for (int i = 0; i < row; i++)
         {
-            matrix[i] = new char[col];
+            array[i] = new char[col];
         }
 
         for (int i = 0; i < row; i++)
         {
             for (int j = 0; j < col; j++)
             {
-                matrix[i][j] = src.matrix[i][j];
+                array[i][j] = src.array[i][j];
             }
         }
     }
+
     ~BinaryImg()
     {
         for (int i = 0; i < row; i++)
         {
-            delete[]matrix[i];
+            delete[]array[i];
         }
-        delete[]matrix;
+        delete[]array;
     }
+
     int GetRow() const { return row; }
     int GetCol() const { return col; }
+
     friend std::ostream& operator<<(std::ostream& os, BinaryImg<char>& obj)
     {
         for (int i = 0; i < obj.row; i++)
         {
             for (int j = 0; j < obj.col; j++)
             {
-                std::cout << obj.matrix[i][j] << std::setw(2);
+                std::cout << obj.array[i][j] << std::setw(2);
             }
             std::cout << "\n";
         }
         return os;
     }
+
     bool operator==(const BinaryImg<char>& src) const
     {
         if (row != src.row || col != src.col) { return false; }
@@ -410,65 +415,74 @@ public:
         {
             for (int j = 0; j < col; j++)
             {
-                if (matrix[i][j] != src.matrix[i][j]) { return false; }
+                if (array[i][j] != src.array[i][j]) { return false; }
             }
         }
         return true;
     }
+
     bool operator!=(const BinaryImg<char>& src) const
     {
         return !(*this == src);
     }
+
     char& operator()(int x, int y)
     {
         if ((x >= row || y >= col) || (x < 0 || y < 0))
         {
             throw EClassException("invalid index");
         }
-        return matrix[x][y];
+        return array[x][y];
     }
+
     char operator()(int x, int y) const
     {
         if ((x >= row || y >= col) || (x < 0 || y < 0))
         {
             throw EClassException("invalid index");
         }
-        return matrix[x][y];
+        return array[x][y];
     }
+
     BinaryImg<char> operator+(const BinaryImg<char>& src) const
     {
         return this->Or(src);
     }
+
     BinaryImg<char> operator*(const BinaryImg<char>& src) const
     {
         return this->And(src);
     }
+
     BinaryImg<char> operator+(char rhs) const
     {
         return this->Or(rhs);
     }
+
     BinaryImg<char> operator*(char rhs) const
     {
         return this->And(rhs);
     }
+
     BinaryImg<char> operator!()
     {
         for (int i = 0; i < row; i++)
         {
             for (int j = 0; j < col; j++)
             {
-                if (matrix[i][j] == '0')
+                if (array[i][j] == '0')
                 {
-                    matrix[i][j] = std::numeric_limits<char>::max();
+                    array[i][j] = std::numeric_limits<char>::max();
                 }
                 else
                 {
-                    matrix[i][j] = '0';
+                    array[i][j] = '0';
                 }
             }
         }
         return *this;
     }
+
     BinaryImg<char>& operator= (const BinaryImg<char>& src)
     {
         if (this == (&src)) { return *this; }
@@ -478,7 +492,7 @@ public:
             {
                 for (int j = 0; j < col; j++)
                 {
-                    matrix[i][j] = src.matrix[i][j];
+                    array[i][j] = src.array[i][j];
                 }
             }
             return *this;
@@ -486,28 +500,29 @@ public:
 
         for (int i = 0; i < row; i++)
         {
-            delete[]matrix[i];
+            delete[]array[i];
         }
-        delete[]matrix;
+        delete[]array;
 
         row = src.row;
         col = src.col;
 
-        matrix = new char* [row];
+        array = new char* [row];
         for (int i = 0; i < row; i++)
         {
-            matrix[i] = new char[col];
+            array[i] = new char[col];
         }
 
         for (int i = 0; i < row; i++)
         {
             for (int j = 0; j < col; j++)
             {
-                matrix[i][j] = src.matrix[i][j];
+                array[i][j] = src.array[i][j];
             }
         }
         return *this;
     }
+
     double FillFactor() const
     {
         int k = 0;
@@ -515,16 +530,18 @@ public:
         {
             for (int j = 0; j < col; j++)
             {
-                if (matrix[i][j] != '0') { k++; }
+                if (array[i][j] != '0') { k++; }
             }
         }
         double res = (double)k / (row * col);
         return res;
     }
+
     friend BinaryImg<char> operator*(char rhs, const BinaryImg<char>& src)
     {
         return src.And(rhs);
     }
+
     friend BinaryImg<char> operator+(char rhs, const BinaryImg<char>& src)
     {
         return src.Or(rhs);
